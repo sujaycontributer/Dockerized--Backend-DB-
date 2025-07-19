57baff4f32f0312cf504f6b407c9bb4f584793d8
Sujay Ghosh <sujayg222@gmail.com>
2025-07-19 21:36:59 +0530

    Done!

--- src/db.ts
+++ src/db.ts
@@ -1,6 +1,6 @@
 import mongoose, { Schema, model } from 'mongoose';
 
-const mongoUrl: string = 'mongodb://localhost:27018/myDatabase';
+const mongoUrl: string = 'mongodb://mongodbcontainer:27017/myDatabase';
 
 // Connect to MongoDB
 mongoose.connect(mongoUrl)
